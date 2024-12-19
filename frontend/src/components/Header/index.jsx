e294e97af89f3f7b8ff74df5455663b31fb3f482
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-12-19 15:45:36 +0100

    Recréer front react avec vite

--- frontend/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function header () {
-  return (
-    <header>
-      <h1>Header</h1>
-    </header>
-  );
-}
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import '../../styles/Home.css'
+
+function Home() {
+  const name = "John Doe"
+  return (
+    <>
+      <h1>Bienvenue. {name}</h1>
+    </>
+  )
+}
+
+export default Home
